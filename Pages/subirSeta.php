9109202c6c67b86ec9705431b5b05fa0fc145230
ItzKTZ <ItzKTZ@gmail.com>
2025-05-05 23:33:52 +0200

    refactor bugfix 1

--- Pages/subirSeta.php
+++ Pages/subirSeta.php
@@ -37,92 +37,67 @@
 
 <header>
     <div class="header-top">
-        <!-- Imagen con el logo de Somican que al pinchar sobre él te lleva a la página principal de Somican -->
-        <a href="https://somican.com/#">
-            <img class="logo" src="../Images/Logo.png" alt="Logo Somican" />
-        </a>
-        <!-- Botón para acceder como socio a la página web -->
-        <a id="access-button" href="./accesoSocios.php">Acceso Socios</a>
-        <p id="header-title">Sociedad Micológica Cántabra</p>
+        <div class="left">
+            <a href="">
+                <img src="../Images/Logo.png" alt="Logo Somican"/>
+            </a>
+        </div>
+        <div class="middle">
+            <h1>Sociedad Micológica Cántabra</h1>
+            <a href="./accesoSocios.php" id="access-button">Acceso Socios</a>
+        </div>
+        <div class="right">
+            <!-- Botón que al pinchar sobre el te redirige al Facebook de Somican -->
+            <a href="https://www.facebook.com/sociedad.micologicacantabra?locale=es_ES">
+                <!-- SVG's de facebook, youtube y email para no perder calidad -->
+                <svg class="facebook" viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
+                </svg>
+            </a>
+            <!-- Botón que al pinchar sobre el te redirige al canal de Youtube de Somican -->
+            <a href="https://www.youtube.com/@SociedadMicologicaCantabra">
+                <svg class="youtube" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path>
+                </svg>
+            </a>
+            <!-- Botón que al pinchar sobre el te redirige a la pgina de contacto de Somican -->
+            <a href="https://somican.com/contacto/">
+                <svg class="email" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="currentColor" d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
+                </svg>
+            </a>
+        </div>
     </div>
 
-    <nav>
-        <ul>
-            <li>
-                <div class="header-dropdown">
-                    <!-- Boton que se despliega al tener el cursor sobre él -->
-                    <button>SOMICAN +</button>
-                    <div class="header-dropdown-content">
-                        <!-- Botón que al pinchar sobre él te redirige a la pagina de Somican de sus noticias -->
-                        <a href="https://somican.com/blog/">NOTICIAS</a>
-                        <!-- Botón que al pinchar sobre él te redirige a la pagina de Somican de su historia -->
-                        <a href="https://somican.com/historia-la-sociedad-micologica-cantabra/">HISTORIA</a>
-                        <!-- Botón que al pinchar sobre él te redirige a la pagina de Somican para consultar informacion respecto a los socios -->
-                        <a href="https://somican.com/socios-la-sociedad-micologica-cantabra/">SOCIOS</a>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <div class="header-dropdown">
-                    <!-- Boton que se despliega al tener el cursor sobre él -->
-                    <button>GALERÍAS +</button>
-                    <div class="header-dropdown-content">
-                        <!-- Botón que al pinchar sobre él te redirige a nuestra Galería Artística -->
-                        <a href="../index.php">GALERIA ARTÍSTICA</a>
-                        <!-- Botón que al pinchar sobre él te redirige a nuestra Galeria Cientifica (será necesario acceder como socio para verla) -->
-                        <a href="../Pages/galeriaCientifica.php">GALERIA CIENTIFICA</a>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <!-- Botón que al pinchar sobre él te redirige a la pagina de Somican de la Revista Yesca -->
-                <a href="https://somican.com/revista-micologia-yesca/"><button>REVISTA YESCA</button></a>
-            </li>
-            <li>
-                <div class="header-dropdown">
-                    <!-- Boton que se despliega al tener el cursor sobre él -->
-                    <button>MEDIOS +</button>
-                    <div class="header-dropdown-content">
-                        <!-- Botón que al pinchar sobre él te redirige a la pagina de Somican de su biblioteca -->
-                        <a href="https://somican.com/biblioteca-micologia/">BIBLIOTECA</a>
-                        <!-- Botón que al pinchar sobre él te redirige a la pagina de Somican de sus enlaces de interés -->
-                        <a href="https://somican.com/enlaces-interes-micologia/">ENLACES DE INTERÉS</a>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <!-- Botón que al pinchar sobre el te redirige a la página de contacto de Somican -->
-                <a href="https://somican.com/contacto/"><button>CONTACTO</button></a>
-            </li>
-        </ul>
-    </nav>
-
-    <div class="social-media">
-        <!-- Botón que al pinchar sobre el te redirige al Facebook de Somican -->
-        <a href="https://www.facebook.com/sociedad.micologicacantabra?locale=es_ES">
-            <!-- SVG's de facebook, youtube y email para no perder calidad -->
-            <svg class="facebook" viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
-                </path>
-            </svg>
-        </a>
-        <!-- Botón que al pinchar sobre el te redirige al canal de Youtube de Somican -->
-        <a href="https://www.youtube.com/@SociedadMicologicaCantabra">
-            <svg class="youtube" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z">
-                </path>
-            </svg>
-        </a>
-        <!-- Botón que al pinchar sobre el te redirige a la pgina de contacto de Somican -->
-        <a href="https://somican.com/contacto/">
-            <svg class="email" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z">
-                </path>
-            </svg>
-        </a>
+    <div class="header-bottom">
+        <div class="dropdown">
+            <h2>Somican +</h2>
+            <div class="dropdown-content">
+                <a href="https://somican.com/blog/">Noticias</a>
+                <a href="https://somican.com/historia-la-sociedad-micologica-cantabra/">Historia</a>
+                <a href="https://somican.com/socios-la-sociedad-micologica-cantabra/">Socios</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <h2>Galerías +</h2>
+            <div class="dropdown-content">
+                <a href="../index.php">Galería Artística</a>
+                <a href="./galeriaCientifica.php">Galería Científica</a>
+            </div>
+        </div>
+        <div class="button">
+            <a href="https://somican.com/revista-micologia-yesca">Revista Yesca</a>
+        </div>
+        <div class="dropdown">
+            <h2>Medios +</h2>
+            <div class="dropdown-content">
+                <a href="https://somican.com/biblioteca-micologia">Biblioteca</a>
+                <a href="https://somican.com/enlaces-interes-micologia">Enlaces de Interés</a>
+            </div>
+        </div>
+        <div class="button">
+            <a href="https://somican.com/contacto">Contacto</a>
+        </div>
     </div>
 </header>
 
