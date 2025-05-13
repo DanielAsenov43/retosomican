28ae28263b5cdf5f8e58dacb7016444b94c46ae5
Daniel Asénov <dasenovp01@educantabria.es>
2025-05-13 12:08:19 +0200

    Refactor: changeEmail, password y login.php

--- PHPScripts/changeEmail.php
+++ PHPScripts/changeEmail.php
@@ -1,5 +1,9 @@
 <?php
+// Ejecutar el script "connection.php" que crea una sesión y guarda la conexión en $_SESSION["SQL"]
 include "./connection.php";
+$connection = $_SESSION["SQL"];
+
+// Constantes con los distintos mensajes de error o éxito, se pueden cambiar
 $ERROR_SAME_EMAIL = "¡El correo nuevo debe ser distinto!";
 $ERROR_OLD_EMAIL_DOES_NOT_MATCH = "¡El correo actual no coincide!";
 $ERROR_NEW_EMAIL_IN_USE = "¡El correo nuevo no es válido!";
@@ -7,44 +11,57 @@ $ERROR_PASSWORD_DOES_NOT_MATCH = "¡La contraseña no es correcta!";
 $ERROR_DDBB_CONNECTION = "¡Se ha producido un error interno!";
 $CHANGE_SUCCESS = "¡El correo se ha actualizado!";
 
-$connection = $_SESSION["SQL"];
-
-// POST INFO
+// Información del formulario de cambio de correo
 $oldEmail = $_POST["email-old"];
 $newEmail = $_POST["email-new"];
 $password = $_POST["email-password"];
 
+// COMPROBACIONES =================================================================
+
+// Si el correo antiguo no coincide con el correo de confirmación del formulario, mostrar un error
 if(!compare($oldEmail, $_SESSION["USER-EMAIL"])) {
     setResult($ERROR_OLD_EMAIL_DOES_NOT_MATCH, true);
     return;
 }
+// Si el correo antiguo y el correo nuevo son iguales, mostrar un error ya que el correo nuevo debe ser distinto
 if(compare($oldEmail, $newEmail)) {
     setResult($ERROR_SAME_EMAIL, true);
     return;
 }
+// Si la contraseña del formulario no coincide con la contraseña de socio, mostrar un error
 if(!password_verify($password, $_SESSION["USER-PASSWORD"])) {
     setResult($ERROR_PASSWORD_DOES_NOT_MATCH, true);
     return;
 }
-
+// Crear una consulta que obtiene todos los socios con el correo nuevo. Si hay más de 0 socios con ese correo, mostrar un error.
 $userEmailsQuery = "SELECT * FROM retosomican.socios WHERE email LIKE '$newEmail'";
 $result = mysqli_query($connection, $userEmailsQuery);
-if(mysqli_num_rows($result) > 0) {
+if(mysqli_num_rows($result) > 0) { // Comprobar si hay algún socio con el correo nuevo. En caso positivo, mostrar un error
     setResult($ERROR_NEW_EMAIL_IN_USE, true);
     return;
 }
 
+// FIN DE COMPROBACIONES ===========================================================
+
+// Creamos la consulta para cambiar el correo del socio
 $changeEmailQuery = "UPDATE retosomican.socios SET email = '$newEmail' WHERE ID = " . $_SESSION["USER-ID"];
+// Ejecutamos la consulta comprobando si surgen errores
 if($connection -> query($changeEmailQuery)) {
+    // Si no hay errores, mostramos un mensaje de éxito y actualizamos el correo de la sesión
     setResult($CHANGE_SUCCESS, false);
     $_SESSION["USER-EMAIL"] = $newEmail;
 }
+// En caso contrario, mostrar un error de conexión con la BBDD
 else setResult($ERROR_DDBB_CONNECTION, true);
 
+// FUNCIONES =====================================================
+
+// Función que compara dos cadenas ignorando mayúsculas, utilizado mayoritariamente para correos
 function compare($string1, $string2) {
     return strtolower($string1) == strtolower($string2);
 }
 
+// Función que le establece un valor a $_SESSION["CHANGE-RESULT"] dependiendo de si $isError es verdadero o falso, y te lleva a la página del perfil.
 function setResult($message, $isError) { 
     if($isError) $_SESSION["CHANGE-RESULT"] = "<span class='error'>$message</span>";
     else $_SESSION["CHANGE-RESULT"] = "<span class='success'>$message</span>";
--- PHPScripts/changePassword.php
+++ PHPScripts/changePassword.php
@@ -1,53 +1,73 @@
 <?php
+// Ejecutar el script "connection.php" que crea una sesión y guarda la conexión en $_SESSION["SQL"]
 include "./connection.php";
 
+// Constantes con los distintos mensajes de error o éxito, se pueden cambiar
 $ERROR_SAME_PASSWORD = "¡La contraseña nueva debe ser distinta de la actual!";
 $ERROR_OLD_PASSWORD_DOES_NOT_MATCH = "¡La contraseña actual no coincide!";
 $ERROR_PASSWORD_DOES_NOT_MATCH = "¡Las contraseñas nuevas no coinciden!";
+$ERROR_PASSWORD_CONTAINS_SPACES = "¡La contraseña no debe contener espacios!";
+$ERROR_PASSWORD_MUST_CONTAIN_UPPERCASE = "¡La contraseña debe contener al menos una letra mayúscula!";
 $ERROR_DDBB_CONNECTION = "¡Se ha producido un error interno!";
 $CHANGE_SUCCESS = "¡La contraseña se ha actualizado!";
 
-$connection = $_SESSION["SQL"];
-
-// POST INFO
+// Información del formulario de cambio de contraseña
 $oldPassword = $_POST["password-old"];
 $newPassword = $_POST["password-new"];
 $newPasswordConfirm = $_POST["password-new-confirm"];
 
+// COMPROBACIONES =================================================================
+
+// Si la contraseña del socio no coincide con la contraseña introducida en el formulario, mostrar un error
 if(!password_verify($oldPassword, $_SESSION["USER-PASSWORD"])) {
     setResult($ERROR_OLD_PASSWORD_DOES_NOT_MATCH, true);
     return;
 }
+// Si la contraseña nueva es igual que la antigua, mostrar un error.
 if($oldPassword == $newPassword) {
     setResult($ERROR_SAME_PASSWORD, true);
     return;
 }
-
+// Si la contraseña contiene un espacio, mostrar un error
 if(str_contains($newPassword, " ")) {
-    setResult("¡La contraseña no debe contener espacios!", true);
+    setResult($ERROR_PASSWORD_CONTAINS_SPACES, true);
     return;
 }
+// Si la contraseña es igual que la contraseña en minúscula, mostrar un mensaje de error porque debe contener al menos una mayúscula.
+// Este filtro también comprueba si la contraseña son solo números, ya que "123" es igual que "123" en minúsculas y mostrará un error.
 if($newPassword == strtolower($newPassword)) {
-    setResult("¡La contraseña debe contener al menos una letra mayúscula!", true);
+    setResult($ERROR_PASSWORD_MUST_CONTAIN_UPPERCASE, true);
     return;
 }
-
+// Si las contraseñas nuevas no coinciden, mostrar un error.
 if($newPassword != $newPasswordConfirm) {
     setResult($ERROR_PASSWORD_DOES_NOT_MATCH, true);
     return;
 }
 
+// FIN DE COMPROBACIONES ===========================================================
+
+// Encriptamos la contraseña nueva antes de enviarla a la base de datos
 $newPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+
+// Creamos la línea de actualización de SQL que le cambia la contraseña al socio
 $changePasswordQuery = "UPDATE retosomican.socios SET contrasenia = '$newPassword' WHERE ID = " . $_SESSION["USER-ID"];
+// Ejecutamos la actualización y comprobamos si da error.
 if($connection -> query($changePasswordQuery)) {
+    // Si no da error, mostramos un mensaje de cambio exitoso y apuntamos la nueva contraseña en la sesión.
     setResult($CHANGE_SUCCESS, false);
     $_SESSION["USER-PASSWORD"] = $newPassword;
 }
+// Si da error, enviamos un mensaje de error y volvemos al perfil
 else setResult($ERROR_DDBB_CONNECTION, true);
+// Después del cambio de contraseña, volvemos al perfil del socio
+header("location: ../Pages/profile.php");
 
-function setResult($message, $isError) { 
+// FUNCIONES =======================================================================
+
+// Función que cambia la variable de sesión "CHANGE-RESULT", y añade un span cuya clase cambia dependiendo de $isError
+function setResult($message, $isError) {
     if($isError) $_SESSION["CHANGE-RESULT"] = "<span class='error'>$message</span>";
     else $_SESSION["CHANGE-RESULT"] = "<span class='success'>$message</span>";
-    header("location: ../Pages/profile.php");
 }
 ?>
